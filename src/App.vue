8f96cc9dead29e0b594a8a7353923fb8d8cab64c
edyywang <edyywang@gmail.com>
2017-01-07 01:30:17 +0800

    Customer Component and Data methods

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
   <div id="app">
     <message>Hello There</message>
     <message>Hello There Again</message>
+    <counter></counter>
   </div>
 </template>
 
 <script>
+// 需要import後註冊
 import Message from './components/Message.vue';
+import Counter from './components/Counter.vue';
+
 export default {
   name: 'app',
-  components: { Message },
+  components: { Message, Counter }, // 需要註冊components
   data () {
     return {
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <h2>Count: {{ count }}</h2>
+    <p><button @click="count +=1">increment</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  //如同Vue Component的data方法部分, 需要回傳一個物件
+  data() {
+    return { count: 0 };
+  }
+}
+</script>
+
+<style lang="css">
+</style>
